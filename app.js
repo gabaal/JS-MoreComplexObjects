100841bc8b9c20f661eb7c3e366fa14d88bfc273
gabaal <geraldbaalham@live.co.uk>
2024-01-16 12:56:05 +0000

    Complete

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log("Working");
+//console.log("Working");
+
+let cars = [
+  {
+    color: "purple",
+    type: "minivan",
+    registration: new Date("2017-01-03"),
+    capacity: 7,
+  },
+  {
+    color: "red",
+    type: "station wagon",
+    registration: new Date("2018-03-03"),
+    capacity: 5,
+  },
+];
+//console.log(cars);
+//add car to first position
+// let car = {
+//   color: "red",
+//   type: "cabrio",
+//   registration: new Date("2016-05-02"),
+//   capacity: 2,
+// };
+//cars.unshift(car);
+//console.log(cars);
+
+//add car to last position
+// let car = {
+//   color: "red",
+//   type: "cabrio",
+//   registration: new Date("2016-05-02"),
+//   capacity: 2,
+// };
+// cars.push(car);
+// console.log(cars);
+
+//Add a new object in the middle - Array.splice
+
+// let car = {
+//   color: "red",
+//   type: "cabrio",
+//   registration: new Date("2016-05-02"),
+//   capacity: 2,
+// };
+// cars.splice(4, 0, car);
+// console.log(cars);
+
+let car = cars.find((car) => car.color === "red");
+console.log(cars);
